c3f5a85fff7dd736fec34aedcd008e8e0143ffd0
chenp6 <cpr40517@gmail.com>
2023-03-09 13:52:24 +0800

    【connection】優化資料庫連線方式

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ dotenv.config();
 
 let connectStatus = "closed";
 
-let examTable;
-let groupTable;
-let processTable;
-let updateTimeTable;
 const uri = process.env.MONGODB_URL;
 
-// Create a new MongoClient
-const client = new MongoClient(uri);
+
 
 async function run() {
     connectStatus = "start connect";
@@ -27,15 +22,31 @@ async function run() {
     await db.command({ ping: 1 });
 
     connectStatus = "start table collection connection";
-    examTable = await db.collection("exam", { tls: true });
-    groupTable = await db.collection("group", { tls: true });
-    processTable = await db.collection("process", { tls: true });
-    updateTimeTable = await db.collection("update time", { tls: true });
+    // examTable = await connectTable("exam", { tls: true });
+    // groupTable = await connectTable("group", { tls: true });
+    // processTable = await connectTable("process", { tls: true });
+    // updateTimeTable = await connectTable("update time", { tls: true });
 
     connectStatus = "ok";
     console.log(new Date() + "資料庫資料完成連接")
 }
 run().catch(console.dir);
+
+
+async function connectTable(table) {
+
+    // Create a new MongoClient
+    const client = new MongoClient(uri);
+
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    // Establish and verify connection
+    const db = client.db("reserveProcess");
+    const collection = await db.collection(table, { tls: true });
+    return collection;
+}
+
 
 let app = express();
 
@@ -51,6 +62,7 @@ app.listen(3000 || process.env.PORT, () => {
 
 app.get("/getExamSelect", async(req, res) => {
     try {
+        const examTable = await connectTable("exam");
         const examList = await examTable.find({ school: req.query.school }).toArray();
         return res.status(200).json(examList);
     } catch (error) {
@@ -64,6 +76,7 @@ app.get("/getExamSelect", async(req, res) => {
 app.get("/getGroupSelect", async(req, res) => {
     //group list
     try {
+        const groupTable = await connectTable("group", { tls: true });
         return res.status(200).json(await groupTable.find({ school: req.query.school, examNo: '' + req.query.examNo }).toArray());
     } catch (error) {
         return res.status(500).json({
@@ -75,6 +88,7 @@ app.get("/getGroupSelect", async(req, res) => {
 app.get("/getReserveProcess", async(req, res) => {
     //group list
     try {
+        const groupTable = await connectTable("group", { tls: true });
         return res.status(200).json(await groupTable.findOne({ school: req.query.school, examNo: '' + req.query.examNo, groupNo: '' + req.query.groupNo }));
     } catch (error) {
         return res.status(500).json({
@@ -86,6 +100,7 @@ app.get("/getReserveProcess", async(req, res) => {
 app.get("/getUserRank", async(req, res) => {
     //user rank
     try {
+        const processTable = await connectTable("process", { tls: true });
         return res.status(200).json(await processTable.findOne({ groupId: req.query.groupId, userId: req.query.userId }));
     } catch (error) {
         return res.status(500).json({
@@ -94,19 +109,20 @@ app.get("/getUserRank", async(req, res) => {
     }
 });
 
-app.get("/getStatus", async(req, res) => {
-    //user rank
-    try {
-        return res.status(200).json({ status: connectStatus })
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
-    }
-});
+// app.get("/getStatus", async(req, res) => {
+//     //user rank
+//     try {
+//         return res.status(200).json({ status: connectStatus })
+//     } catch (error) {
+//         return res.status(500).json({
+//             result: null
+//         })
+//     }
+// });
 
 app.get("/getUpdateTime", async(req, res) => {
     try {
+        const updateTimeTable = await connectTable("updateTime", { tls: true });
         return res.status(200).json(await updateTimeTable.findOne({ school: req.query.school, examNo: req.query.examNo }));
     } catch (error) {
         return res.status(500).json({
--- index_local.js
+++ index_local.js
@@ -6,36 +6,47 @@ dotenv.config();
 
 let connectStatus = "closed";
 
-let examTable;
-let groupTable;
-let processTable;
-let updateTimeTable;
 const uri = process.env.MONGODB_URL;
 
-// Create a new MongoClient
-const client = new MongoClient(uri);
 
-async function run() {
-    connectStatus = "start connect";
+
+// async function run() {
+//     connectStatus = "start connect";
+
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+
+//     connectStatus = "start Establish and verify connection";
+//     // Establish and verify connection
+//     const db = client.db("reserveProcess");
+//     await db.command({ ping: 1 });
+
+//     connectStatus = "start table collection connection";
+//     // examTable = await connectTable("exam", { tls: true });
+//     // groupTable = await connectTable("group", { tls: true });
+//     // processTable = await connectTable("process", { tls: true });
+//     // updateTimeTable = await connectTable("update time", { tls: true });
+
+//     connectStatus = "ok";
+//     console.log(new Date() + "資料庫資料完成連接")
+// }
+// run().catch(console.dir);
+
+
+async function connectTable(table) {
+
+    // Create a new MongoClient
+    const client = new MongoClient(uri);
 
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    connectStatus = "start Establish and verify connection";
     // Establish and verify connection
     const db = client.db("reserveProcess");
-    await db.command({ ping: 1 });
-
-    connectStatus = "start table collection connection";
-    examTable = await db.collection("exam", { tls: true });
-    groupTable = await db.collection("group", { tls: true });
-    processTable = await db.collection("process", { tls: true });
-    updateTimeTable = await db.collection("update time", { tls: true });
-
-    connectStatus = "ok";
-    console.log(new Date() + "資料庫資料完成連接")
+    const collection = await db.collection(table, { tls: true });
+    return collection;
 }
-run().catch(console.dir);
+
 
 let app = express();
 
@@ -51,6 +62,7 @@ app.listen(3000 || process.env.PORT, () => {
 
 app.get("/getExamSelect", async(req, res) => {
     try {
+        const examTable = await connectTable("exam");
         const examList = await examTable.find({ school: req.query.school }).toArray();
         return res.status(200).json(examList);
     } catch (error) {
@@ -64,6 +76,7 @@ app.get("/getExamSelect", async(req, res) => {
 app.get("/getGroupSelect", async(req, res) => {
     //group list
     try {
+        const groupTable = await connectTable("group", { tls: true });
         return res.status(200).json(await groupTable.find({ school: req.query.school, examNo: '' + req.query.examNo }).toArray());
     } catch (error) {
         return res.status(500).json({
@@ -72,10 +85,10 @@ app.get("/getGroupSelect", async(req, res) => {
     }
 });
 
-
 app.get("/getReserveProcess", async(req, res) => {
     //group list
     try {
+        const groupTable = await connectTable("group", { tls: true });
         return res.status(200).json(await groupTable.findOne({ school: req.query.school, examNo: '' + req.query.examNo, groupNo: '' + req.query.groupNo }));
     } catch (error) {
         return res.status(500).json({
@@ -84,10 +97,10 @@ app.get("/getReserveProcess", async(req, res) => {
     }
 });
 
-
 app.get("/getUserRank", async(req, res) => {
     //user rank
     try {
+        const processTable = await connectTable("process", { tls: true });
         return res.status(200).json(await processTable.findOne({ groupId: req.query.groupId, userId: req.query.userId }));
     } catch (error) {
         return res.status(500).json({
@@ -96,20 +109,20 @@ app.get("/getUserRank", async(req, res) => {
     }
 });
 
-app.get("/getStatus", async(req, res) => {
-    //user rank
-    try {
-        return res.status(200).json({ status: connectStatus })
-    } catch (error) {
-        return res.status(500).json({
-            result: null
-        })
-    }
-});
+// app.get("/getStatus", async(req, res) => {
+//     //user rank
+//     try {
+//         return res.status(200).json({ status: connectStatus })
+//     } catch (error) {
+//         return res.status(500).json({
+//             result: null
+//         })
+//     }
+// });
 
 app.get("/getUpdateTime", async(req, res) => {
-
     try {
+        const updateTimeTable = await connectTable("updateTime", { tls: true });
         return res.status(200).json(await updateTimeTable.findOne({ school: req.query.school, examNo: req.query.examNo }));
     } catch (error) {
         return res.status(500).json({
