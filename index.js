cc15d085bb41e34508406e5152537eca850e3768
chenp6 <cpr40517@gmail.com>
2024-11-30 03:26:11 +0800

    test

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use('/', express.static(__dirname + '/**')); //將整個server資料夾放到server上的/路徑
+app.use('/', express.static(__dirname + '/public')); //將整個server資料夾放到server上的/路徑
 //app.use('/css', express.static(__dirname + '/css')); //將整個server資料夾放到server上的/路徑
 
 
